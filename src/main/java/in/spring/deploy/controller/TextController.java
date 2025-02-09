4ecb145e3d92643a68342f291f478161358ef2dc
Ankush Gautam <159611550+ERankushgautam@users.noreply.github.com>
2025-02-09 19:44:31 +0530

    Update TextController.java

--- src/main/java/in/spring/deploy/controller/TextController.java
+++ src/main/java/in/spring/deploy/controller/TextController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TextController {
 
-<<<<<<< HEAD
+
 	@GetMapping("/send")
-=======
+
 	@GetMapping("/txt")
->>>>>>> 1f70e8a2bfb405bd03de323739ee38bb59c80b39
+
 	public ResponseEntity<?> text(){
 		return ResponseEntity.ok("hey.. i'm here after deploy");
 	}
